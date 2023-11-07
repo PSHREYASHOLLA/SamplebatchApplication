92e41f6c35c9e4b6512c50dde78d94cef1307525
PSHREYASHOLLA <pshreyasholla@gmail.com>
2023-11-07 16:31:05 +0530

    Update EmployeeTaxCalculationProcessor.java

--- src/main/java/com/example/postgresql/model/EmployeeTaxCalculationProcessor.java
+++ src/main/java/com/example/postgresql/model/EmployeeTaxCalculationProcessor.java
@@ -1,7 +1,5 @@
 package com.example.postgresql.model;
 
-import java.util.concurrent.TimeUnit;
-
 import org.springframework.stereotype.Component;
 
 /**
@@ -9,6 +7,8 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class EmployeeTaxCalculationProcessor extends AbstractItemProcessor<EmployeeDetail, EmployeeTaxDetail> {
+	
+	private static int counter=1;
 
   /**
    * Execute process.
@@ -19,12 +19,20 @@ public class EmployeeTaxCalculationProcessor extends AbstractItemProcessor<Emplo
    */
   @Override
   protected EmployeeTaxDetail executeProcess(EmployeeDetail item) throws Exception {
+	  System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+		System.out.println(item.getEmpId());
+		System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");  
+	  
     EmployeeTaxDetail employeeTaxDetail = new EmployeeTaxDetail();
     employeeTaxDetail.setEmpId(item.getEmpId());
     employeeTaxDetail.setTaxAmount((long) (item.getEmpSalary() * .1));
-   if(item.getEmpId().equals("emp0032"))
+    if(item.getEmpId().equals("emp0032"))
     {
-    	TimeUnit.SECONDS.sleep(30);
+    	if(counter==1)
+    	{
+    		counter++;
+    		throw new Exception();
+    	}
     }
     return employeeTaxDetail;
   }
